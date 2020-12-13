0074660ae272132e5f75071c3723c732fcff3eeb
Niklas Enns <niklas.enns@gmail.com>
2020-12-13 19:51:34 +0100

    added e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { Appointment } from '../src/appointment.entity';
+import { AppointmentService } from '../src/appointment.service';
+import { CreateAppointment } from '../src/appointment.request';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -10,15 +13,30 @@ describe('AppController (e2e)', () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('Get all appointments', () => {
+    const appointmentService = app.get(AppointmentService);
+    appointmentService.create({
+      participants: [1, 2, 3],
+      name: 'meet in the park',
+    });
+
+    appointmentService.create({
+      participants: [1, 2, 3],
+      name: 'meet in the cinema',
+    });
+
     return request(app.getHttpServer())
-      .get('/')
-      .expect(201)
-      .expect('Hello World!');
+      .get('/appointments')
+      .expect((res) => {
+        expect(res.body).toHaveLength(2);
+      });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
